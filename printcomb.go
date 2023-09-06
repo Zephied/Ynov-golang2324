0f7355b1cce91ad1ed10b721d1867f4159996d77
ganicolas <nicolas.galmiche@ynov.com>
2023-09-06 17:44:34 +0200

    comb

---
+++ printcomb.go
@@ -0,0 +1,26 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	x := '0'
+
+	for x <= '9' {
+		y := '0'
+		for y <= '9' {
+			z := '0'
+			for z <= '9' {
+				if x != y && y != z && x != z && x < y && y < z {
+					z01.PrintRune(rune(x))
+					z01.PrintRune(rune(y))
+					z01.PrintRune(rune(z))
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+				z++
+			}
+			y++
+		}
+		x++
+	}
+}
