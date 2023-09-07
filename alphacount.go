40aea95a7c84b8803cbed62a84800566f3dd58f4
ganicolas <nicolas.galmiche@ynov.com>
2023-09-07 15:06:39 +0200

    a

--- alphacount.go
+++ alphacount.go
@@ -4,7 +4,7 @@ func AlphaCount(s string) int {
 	i := 0
 	c := 0
 	for range s {
-		if s[c] >= '0' && s[c] <= '9' {
+		if s[c] >= 'a' && s[c] <= 'z' || s[c] >= 'A' && s[c] <= 'Z' {
 			i++
 		}
 		c++
