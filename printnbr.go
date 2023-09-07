fa48913539e61b61b07eded80c0de3fababdd2bd
ganicolas <nicolas.galmiche@ynov.com>
2023-09-07 12:11:50 +0200

    nbr4

--- printnbr.go
+++ printnbr.go
@@ -5,23 +5,27 @@ import "github.com/01-edu/z01"
 func PrintNbr(n int) {
 	tab := make([]int, 0)
 	i := 0
-	if n < i {
-		z01.PrintRune('-')
-		for n != 0 {
-			tab = append(tab, n%10*(-1))
-			n = n / 10
-			i++
-		}
+	if n == 0 {
+		z01.PrintRune(rune(n))
 	} else {
-		for n != 0 {
-			tab = append(tab, n%10)
-			n = n / 10
-			i++
+		if n < i {
+			z01.PrintRune('-')
+			for n != 0 {
+				tab = append(tab, n%10*(-1))
+				n = n / 10
+				i++
+			}
+		} else {
+			for n != 0 {
+				tab = append(tab, n%10)
+				n = n / 10
+				i++
+			}
 		}
-	}
-	i--
-	for i >= 0 {
-		z01.PrintRune(rune(tab[i] + '0'))
 		i--
+		for i >= 0 {
+			z01.PrintRune(rune(tab[i] + '0'))
+			i--
+		}
 	}
 }
