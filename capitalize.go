a52f08f45d2f4f7bc55ecd65492c67c2766dd962
ganicolas <nicolas.galmiche@ynov.com>
2023-09-11 11:02:03 +0200

    oui

--- capitalize.go
+++ capitalize.go
@@ -4,22 +4,30 @@ func Capitalize(s string) string {
 	i := 0
 	r := ""
 	for range s {
-		if s[i] == 0 && s[i] <= 'a' && s[i] >= 'z' {
-			r += string(s[i] + 32)
+		if i == 0 && s[i] >= 'a' && s[i] <= 'z' {
+			r += string(s[i] - 32)
+			i++
 		} else {
-			if s[i] == 0 {
-				r += string(s[i])
+			if i == 0 {
+				r += string(s[0])
 				i++
 			}
-			if s[i-1] < '0' && s[i-1] > '9' || s[i-1] < 'a' && s[i-1] > 'z' || s[i-1] < 'A' && s[i-1] > 'Z' {
-				r += string(s[i] + 32)
+			if s[i-1] < '0' || s[i-1] > '9' && s[i-1] < 'A' || s[i-1] > 'Z' && s[i-1] < 'a' || s[i-1] > 'z' {
+				if s[i] >= 'a' && s[i] <= 'z' {
+					r += string(s[i] - 32)
+				} else {
+					r += string(s[i])
+				}
+				i++
 			} else {
 				if s[i] >= 'A' && s[i] <= 'Z' {
-					r += string(s[i] - 32)
+					r += string(s[i] + 32)
+				} else {
+					r += string(s[i])
 				}
+				i++
 			}
 		}
-		i++
 	}
 	return (r)
 }
