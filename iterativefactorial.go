3ef112541c9738867293db2ac35849034f195c12
ganicolas <nicolas.galmiche@ynov.com>
2023-09-12 14:20:32 +0200

    1st try

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,6 +2,9 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	n := 1
+	if nb < 1 || nb > 12 {
+		return (0)
+	}
 	for nb > 0 {
 		n = n * nb
 		nb--
--- main/main.go
+++ main/main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	arg := 4
+	arg := 12
 	fmt.Println(piscine.IterativeFactorial(arg))
 }
