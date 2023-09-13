dea4027987a85fd8757cf7268670154b4fb7c5e2
ganicolas <nicolas.galmiche@ynov.com>
2023-09-13 09:39:09 +0200

    1st try

--- main/main.go
+++ main/main.go
@@ -6,12 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.IsPrime(5))
-	fmt.Println(piscine.IsPrime(4))
-	fmt.Println(piscine.IsPrime(121))
-	fmt.Println(piscine.IsPrime(7521))
-	fmt.Println(piscine.IsPrime(135))
-	fmt.Println(piscine.IsPrime(140))
-	fmt.Println(piscine.IsPrime(17))
-	fmt.Println(piscine.IsPrime(11))
+	arg1 := 7
+	fmt.Println(piscine.Fibonacci(arg1))
 }
