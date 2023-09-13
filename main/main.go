597e448d63ccee4e778ae070a3656dd5eb4ff239
ganicolas <nicolas.galmiche@ynov.com>
2023-09-13 09:15:07 +0200

    1st try

--- main/main.go
+++ main/main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.IterativePower(-5, 4))
+	fmt.Println(piscine.RecursivePower(-5, 4))
 }
--- recursivepower.go
+++ recursivepower.go
@@ -1,16 +1,11 @@
 package piscine
 
 func RecursivePower(nb int, power int) int {
-	ininb := nb
 	if power < 0 {
 		return (0)
 	}
 	if power == 0 {
 		return (1)
 	}
-	for power > 1 {
-		nb = nb * ininb
-		power--
-	}
-	return (nb)
+	return (nb * RecursivePower(nb, power-1))
 }
