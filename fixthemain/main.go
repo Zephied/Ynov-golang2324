ffe637ee6dd185a8ba02d7521040efdcea2792b1
ganicolas <nicolas.galmiche@ynov.com>
2023-09-14 14:19:28 +0200

    first try

--- fixthemain/main.go
+++ fixthemain/main.go
@@ -13,24 +13,24 @@ type Door struct {
 }
 
 func OpenDoor(ptrDoor *Door) bool {
-	PrintStr("Door Opening...")
+	PrintStr("Door Opening...\n")
 	ptrDoor.state = false
 	return false
 }
 
 func CloseDoor(ptrDoor *Door) bool {
-	PrintStr("Door Closing...")
+	PrintStr("Door Closing...\n")
 	ptrDoor.state = true
 	return true
 }
 
 func IsDoorOpen(ptrDoor *Door) bool {
-	PrintStr("is the Door opened ?")
+	PrintStr("is the Door opened ?\n")
 	return ptrDoor.state
 }
 
 func IsDoorClose(ptrDoor *Door) bool {
-	PrintStr("is the Door closed ?")
+	PrintStr("is the Door closed ?\n")
 	return ptrDoor.state
 }
 
