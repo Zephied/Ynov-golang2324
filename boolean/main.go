378a71f4e3527ae312cdd5cafb8a0ad64edd7021
ganicolas <nicolas.galmiche@ynov.com>
2023-09-18 12:09:22 +0200

    first try

---
+++ boolean/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	if nbr == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	EvenMsg := "I have an even number of arguments"
+	OddMsg := "I have an odd number of arguments"
+	i := 0
+	lengthOfArg := 0
+	for range os.Args {
+		i++
+	}
+	if i > 2 {
+		lengthOfArg = 1
+	}
+	if isEven(lengthOfArg) == true {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
