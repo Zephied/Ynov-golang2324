9daeeb75d7aba4637631841a098cc1888ad004e8
ganicolas <nicolas.galmiche@ynov.com>
2023-09-19 11:15:33 +0200

    first try

---
+++ split.go
@@ -0,0 +1,25 @@
+package piscine
+
+func Split(s, sep string) []string {
+	if sep == "" {
+		return []string{s}
+	}
+	var slr []string
+	start := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == sep[0] {
+			if i != start && s[i:start] != "" {
+				slr = append(slr, s[start:i])
+			}
+			start = i + len(sep)
+			if start > len(s) {
+				return slr
+			}
+			if s[i:start] == sep {
+				i = start - 1
+			}
+		}
+	}
+	slr = append(slr, s[start:])
+	return slr
+}
