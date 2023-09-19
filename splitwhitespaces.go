1c4cf05cc3d3a295f06d317835fc640d7a945f53
ganicolas <nicolas.galmiche@ynov.com>
2023-09-19 09:15:16 +0200

    first try

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -5,19 +5,23 @@ func SplitWhiteSpaces(s string) []string {
 	i := 0
 	for range s {
 		j := i
-		if s[j] == 0 && s[i] == ' ' || s[j] == 0 && s[i] == '	' || s[j] == 0 && s[i] == '\n' {
-			i++
-			j++
-		}
-		for s[j] != ' ' && s[j] != '	' && s[j] != '\n' && j != len(s)-1 {
-			j++
-		}
-		tab = append(tab, s[i:j])
-		i = j
-		if s[i] == ' ' || s[i] == '	' || s[i] == '\n' {
-			for s[i] == ' ' || s[i] == '	' || s[i] == '\n' || i == len(s)-1 {
+		if s[i] != ' ' && s[i] != '	' && s[i] != '\n' {
+			if s[j] == 0 && s[i] == ' ' || s[j] == 0 && s[i] == '	' || s[j] == 0 && s[i] == '\n' {
 				i++
+				j++
 			}
+			for s[j] != ' ' && s[j] != '	' && s[j] != '\n' && j != len(s)-1 {
+				j++
+			}
+			tab = append(tab, s[i:j])
+			i = j
+			if s[i] == ' ' || s[i] == '	' || s[i] == '\n' {
+				for s[i] == ' ' || s[i] == '	' || s[i] == '\n' || i == len(s)-1 {
+					i++
+				}
+			}
+		} else {
+			i++
 		}
 	}
 	return tab
