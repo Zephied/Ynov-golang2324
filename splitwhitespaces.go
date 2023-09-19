bffa1725aa8ca2ddfdffd661bb17da3e466de7c9
ganicolas <nicolas.galmiche@ynov.com>
2023-09-19 10:11:28 +0200

    first try

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,26 +1,18 @@
 package piscine
 
 func SplitWhiteSpaces(s string) []string {
-	tab := make([]string, 0)
-	i := 0
-	for range s {
-		j := i
-		if s[i] != ' ' && s[i] != '	' && s[i] != '\n' {
-			for s[j] != ' ' && s[j] != '	' && s[j] != '\n' && j < len(s)-1 {
-				j++
+	start := 0
+	slr := []string{}
+	for i, v := range s {
+		if v == ' ' {
+			if !(s[i] == ' ' && s[start] == ' ') {
+				slr = append(slr, s[start:i])
 			}
-			if i != j {
-				tab = append(tab, s[i:j])
-			}
-			i = j
-			for s[i] == ' ' || s[i] == '	' || s[i] == '\n' || i == len(s) {
-				i++
-			}
-		} else {
-			i++
+			start = i + 1
 		}
 	}
-	return tab
+	slr = append(slr, s[start:])
+	return slr
 }
 
 /*
