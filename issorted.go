08b2680cebcc7ea0df28b221606ba89ba74ad934
ganicolas <nicolas.galmiche@ynov.com>
2023-09-21 16:27:12 +0200

    first try but i think it will never pass it

---
+++ issorted.go
@@ -0,0 +1,24 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	if len(a) == 0 {
+		return false
+	}
+	if len(a) == 1 {
+		return true
+	}
+	if f(a[0], a[1]) < 0 {
+		for i := 1, v := range a, i++ {
+			if f(a[i], a[i+1]) > 0 {
+				return false
+			}
+		}
+	} else {
+		for i := 1, v := range a, i++ {
+			if f(a[i], a[i+1]) < 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
