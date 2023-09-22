b46be0a29e5e3817e7aeb7fa806ec5e131956879
ganicolas <nicolas.galmiche@ynov.com>
2023-09-22 14:28:18 +0200

    2nd try

--- issorted.go
+++ issorted.go
@@ -8,13 +8,13 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 		return true
 	}
 	if f(a[0], a[1]) < 0 {
-		for i := 1, v := range a, i++ {
+		for i := 0; i < len(a)-1; i++ {
 			if f(a[i], a[i+1]) > 0 {
 				return false
 			}
 		}
 	} else {
-		for i := 1, v := range a, i++ {
+		for i := 1; i < len(a)-1; i++ {
 			if f(a[i], a[i+1]) < 0 {
 				return false
 			}
