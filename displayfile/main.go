58fd59bf08097f519c4f7262e4a3c49cd264df8b
ganicolas <nicolas.galmiche@ynov.com>
2023-09-22 15:21:39 +0200

    first try

---
+++ displayfile/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("File name missing")
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+	}
+	file, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	if len(os.Args) == 2 {
+		fmt.Println(string(file))
+	}
+}
