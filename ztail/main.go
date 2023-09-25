7836e01bf7099c41ba9960332ac0e256acdd0082
ganicolas <nicolas.galmiche@ynov.com>
2023-09-25 11:32:48 +0200

    first try

--- ztail/main.go
+++ ztail/main.go
@@ -1,4 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+
+	i := 3
+	var name string
+	start := "==>"
+	end := "<=="
+	var j int
+	if os.Args[1] == "-c" && os.Args[2] >= "0" && os.Args[2] <= "9" {
+		val := 0
+		for _, va := range os.Args[2] {
+			if va >= '0' && va <= '9' {
+				val = val*10 + int(va-'0')
+			}
+		}
+		for i < len(os.Args) {
+			tail := ""
+			count, err := os.ReadFile(os.Args[i])
+			name = os.Args[i]
+			if err != nil {
+				fmt.Printf("err")
+			}
+			fmt.Printf("%s", count)
+			j = val
+			tail = string(count[len(count)-j:])
+			fmt.Printf("%s %s %s \n", start, name, end)
+			fmt.Printf("%s", tail)
+			i++
+		}
+	}
 }
